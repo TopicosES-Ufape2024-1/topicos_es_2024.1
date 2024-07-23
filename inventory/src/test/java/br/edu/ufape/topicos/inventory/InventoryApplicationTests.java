c87de68bf4ef904fc206a861abed4dc8da14e5f4
TayaneCibely <tayane.cibely@ufape.edu>
2024-07-23 16:57:57 -0300

    Feat: Criação do microserviço de estoque

---
+++ inventory/src/test/java/br/edu/ufape/topicos/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.topicos.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
