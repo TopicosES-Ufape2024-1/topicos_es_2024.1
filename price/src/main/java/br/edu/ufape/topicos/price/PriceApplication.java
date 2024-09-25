a657206a00e380e273712e1851cbce535373c300
Alex Lopes <alex.lsilva154@gmail.com>
2024-09-25 01:36:19 -0300

    ajustes na autenticação de price

--- price/src/main/java/br/edu/ufape/topicos/price/PriceApplication.java
+++ price/src/main/java/br/edu/ufape/topicos/price/PriceApplication.java
@@ -3,7 +3,7 @@ package br.edu.ufape.topicos.price;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {"br.edu.ufape.topicos.auth", "br.edu.ufape.topicos.price"})
 public class PriceApplication {
 
 	public static void main(String[] args) {
--- price/src/main/java/br/edu/ufape/topicos/price/controller/PriceController.java
+++ price/src/main/java/br/edu/ufape/topicos/price/controller/PriceController.java
@@ -9,49 +9,61 @@ import br.edu.ufape.topicos.price.model.Price;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/price")
 public class PriceController {
 
     @Autowired
     private PriceFacade priceFacade;
 
 
-    @GetMapping
+    @GetMapping("/price")
+    @PreAuthorize("hasRole('user') or hasRole('manager')")
     public List<PriceResponse> getAllPrices() {
         return priceFacade.getAllPrices().stream().map(PriceResponse::new).toList();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/price/{id}")
+    @PreAuthorize("hasRole('user') or hasRole('manager')")
     public PriceResponse getPriceById(@PathVariable Long id) {
         Price price = priceFacade.getPriceById(id);
         return new PriceResponse(price);
     }
 
-    @GetMapping("/product/{productId}")
+    @GetMapping("/price/product/{productId}")
+    @PreAuthorize("hasRole('user') or hasRole('manager')")
     public PriceResponse getPriceByProductId(@PathVariable Long productId) {
         Price price = priceFacade.getPriceByProductId(productId);
         return new PriceResponse(price);
     }
 
-    @PostMapping
+    @GetMapping("/intranet/price/product/{productId}")
+    public PriceResponse getPriceByProductIdIntranet(@PathVariable Long productId) {
+        Price price = priceFacade.getPriceByProductId(productId);
+        return new PriceResponse(price);
+    }
+
+    @PostMapping("/price")
+    @PreAuthorize("hasRole('manager')")
     public Map<String, String> createPrice(@Valid @RequestBody PriceRequest priceRequest) {
         new PriceResponse(priceFacade.savePrice(priceRequest.toPrice()));
         return Map.of("message", "Price created successfully");
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/price/{id}")
+    @PreAuthorize("hasRole('manager')")
     public Map<String, String> deletePrice(@PathVariable Long id) {
         priceFacade.deletePrice(id);
         return Map.of("message", "Price deleted successfully");
     }
 
-    @GetMapping("/calculate")
+    @GetMapping("/price/calculate")
+    @PreAuthorize("hasRole('user') or hasRole('manager')")
     public ResponseEntity<CalculatePriceResponse> calculateFinalPrice(@Valid @RequestBody CalculatePriceRequest request){
         CalculatePriceResponse finalPrice = priceFacade.calculateFinalPrice(request.productId(), request.quantity());
         return ResponseEntity.ok(finalPrice);
